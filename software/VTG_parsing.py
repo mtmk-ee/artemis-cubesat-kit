f852288c0cee9ab5a19bef24ad1549b59d26af6b
camendol <camendol@hawaii.edu>
2020-06-09 10:55:36 -1000

    Update VTG_parsing.py

--- software/VTG_parsing.py
+++ software/VTG_parsing.py
@@ -1,2 +1,13 @@
 def _parse_GPVTG (self, args)
-  
+    data = args.split(',')
+    if data is None:
+        return
+    
+    self.Tcourse = _parse_float(data[0])
+    self.Treference = _parse_str(data[1])
+    self.Mcourse = _parse_float(data[2])
+    self.Mreference = _parse_str(data[3])
+    self.Ncourse = _parse_float(data[4])
+    self.Nreference = _parse_str(data[5])
+    self.Kcourse = _parse_float(data[6])
+    self.Kreference = _parse_str(data[7])
