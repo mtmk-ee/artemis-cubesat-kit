daf8e5692e2326dc849284901bac0e3194808d00
Chris <camendol@hawaii.edu>
2020-06-23 16:28:28 -1000

    Created payload.py

--- software/payload.py
+++ software/payload.py
@@ -8,6 +8,17 @@ class raspberrypi():
         camera.start_preview()
         sleep(2)
 
+    #take picture in jpg and resize
     def take_pic():
-        
+        camera.capture('foo.jpg', resize=(320,240))
 
+    #makes 60 second long video in h264
+    def take_vid():
+        camera.start_recording('video.h264')
+        camera.wait_recording(60)
+        camera.stop_recording()
+
+    #timelapse with picture taken every five seconds    
+    def timelapse():
+        for filename in camera.capture_continuous('img{counter:03d}.jpg'):
+            sleep(5)
