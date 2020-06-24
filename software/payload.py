49bbe15e15c13fa5fa072ddd46a3db71070a159e
Chris <camendol@hawaii.edu>
2020-06-23 16:21:19 -1000

    Created payload.py

---
+++ software/payload.py
@@ -0,0 +1,13 @@
+from time import sleep
+from picamera import PiCamera 
+
+class raspberrypi():
+    def __init__():
+        camera = PiCamera()
+        camera.resolution(1024, 768)
+        camera.start_preview()
+        sleep(2)
+
+    def take_pic():
+        
+
